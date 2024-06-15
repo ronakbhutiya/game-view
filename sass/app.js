375ff450239882cf82e0f0fe44567a7eff8e35c0
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-15 09:54:15 +0530

    add js

--- sass/app.js
+++ sass/app.js
@@ -1,5 +1,9 @@
 const mobile__nav = document.querySelector(".mobile__nav-btn ");
+const mobile__icon = document.querySelector(".mobile__nav-icon ");
 const nav__header = document.querySelector(".header ");
+let a = true;
 mobile__nav.addEventListener("click", function () {
   nav__header.classList.toggle("nav");
+
+  mobile__nav.style.transform = "rotate(180deg)";
 });
