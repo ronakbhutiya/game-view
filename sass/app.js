31b2752e4290a54327f601b283846fbc827771cd
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-15 11:59:22 +0530

    change nav btn

--- sass/app.js
+++ sass/app.js
@@ -1,9 +1,14 @@
 const mobile__nav = document.querySelector(".mobile__nav-btn ");
 const mobile__icon = document.querySelector(".mobile__nav-icon ");
 const nav__header = document.querySelector(".header ");
-let a = true;
+var a = true;
 mobile__nav.addEventListener("click", function () {
   nav__header.classList.toggle("nav");
-
-  mobile__nav.style.transform = "rotate(180deg)";
+  if (a) {
+    mobile__nav.style.transform = "rotate(180deg)";
+    a = false;
+  } else {
+    a = true;
+    mobile__nav.style.transform = "rotate(360deg)";
+  }
 });
