c0209d0b7a938a91674ddfc45c5827ac187a7beb
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-09-01 10:54:08 +0530

    change most play section

--- sass/app.js
+++ sass/app.js
@@ -1,6 +1,7 @@
 const mobile__nav = document.querySelector(".mobile__nav-btn ");
 const mobile__icon = document.querySelector(".mobile__nav-icon ");
 const nav__header = document.querySelector(".header ");
+const nav__ul = document.querySelector(".nav__ul");
 var a = true;
 mobile__nav.addEventListener("click", function () {
   nav__header.classList.toggle("nav");
@@ -30,3 +31,13 @@ const observer = new IntersectionObserver(
   }
 );
 observer.observe(section__hero);
+const nav__link = document.querySelectorAll(".nav__link");
+nav__ul.addEventListener("click", (e) => {
+  const click = e.target.closest(".nav__link");
+  // console.log(click);
+  nav__link.forEach((cur) => {
+    cur.classList.remove("home");
+  });
+  if (!click) return;
+  click.classList.add("home");
+});
